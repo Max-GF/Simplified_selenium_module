7d76e8b26911753b02be273ee1d7e45a211f49a2
Max-GFilho <125483394+Max-GF@users.noreply.github.com>
2024-11-16 16:20:28 -0300

    alert handle setup

---
+++ modules/custom_selenium/common/alert_handle.py
@@ -0,0 +1,37 @@
+"""
+    Alert handle setup
+"""
+# ---- BASE PYTHON LIBS ----◹
+import time
+# --------------------------◿
+
+# ------------------------ IMPORTED LIBS -----------------------◹
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.alert import Alert
+from selenium.common.exceptions import NoAlertPresentException
+# --------------------------------------------------------------◿
+def accept_alert(web_driver : Chrome,
+                 search_time : float = 5) -> str:
+    """
+        Search for a Alert in WebDriver instace
+        and accept it.
+        Sometime the alet is important, so i get
+        it every time.
+
+    Args:
+        web_driver (Chrome): WebDrive hook
+        search_time (float, optional): How long the search time
+                                       needs to be. Defaults to 5.
+
+    Returns:
+        str: Text present in alert body
+    """
+    while search_time >= 0:
+        try:
+            alert_text : str = Alert(web_driver).text
+            Alert(web_driver).accept()
+            break
+        except NoAlertPresentException:
+            time.sleep(0.1)
+            search_time -= 0.1
+    return {alert_text}
